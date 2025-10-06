bb0a0abc61c4353fb3a854e94d8bc9cfc8d2cb75
Nethara Vidmantha <xnethara@gmail.com>
2025-10-06 10:27:20 +0530

    Install Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       
-      <h1>Nethara Vidmantha</h1>
+      <h1 className='text-red-700'>Nethara Vidmantha</h1>
 
       <ProductCard name="Apple iPad" price="$499" image="https://www.apple.com/assets-www/en_WW/ipad/product_tile/xlarge/ipad_pro_1df5448cd_2x.png"/>
       <ProductCard name="Macbook Air" price="$899" image="https://www.apple.com/assets-www/en_WW/mac/product_tile/xlarge/mba_13_15_2fb165671.png"/>
